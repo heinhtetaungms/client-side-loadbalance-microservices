fc7730e4f5748f621da655c7d9c205ba3bdd7587
heinhtetaung <hha.heinms@gmail.com>
2024-08-07 21:00:31 +0630

    Requests are distributed by the load balancer according to the defined weight function

--- user-service/src/main/java/org/example/userservice/config/SayHelloServiceConfiguration.java
+++ user-service/src/main/java/org/example/userservice/config/SayHelloServiceConfiguration.java
@@ -2,6 +2,7 @@ package org.example.userservice.config;
 
 import org.springframework.cloud.client.DefaultServiceInstance;
 import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.cloud.loadbalancer.core.WeightFunction;
 import org.springframework.cloud.loadbalancer.support.ServiceInstanceListSuppliers;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +13,16 @@ public class SayHelloServiceConfiguration {
 
     @Bean
     public ServiceInstanceListSupplier serviceInstanceListSupplier(ConfigurableApplicationContext context) {
+
+        WeightFunction weightFunction = instance -> {
+            if (instance.getPort() == 8091) {
+                return 4;
+            } else if (instance.getPort() == 8092) {
+                return 1;
+            }
+            return 0;
+        };
+
         ServiceInstanceListSupplier serviceInstanceListSupplier = ServiceInstanceListSuppliers
                 .from("sayhello-service",
                         new DefaultServiceInstance("sayhello-instance1", "sayhello-service", "localhost", 8091, false),
@@ -19,6 +30,7 @@ public class SayHelloServiceConfiguration {
                         );
         return ServiceInstanceListSupplier.builder()
                 .withBase(serviceInstanceListSupplier)
+                .withWeighted(weightFunction)
                 .build(context);
     }
 }
